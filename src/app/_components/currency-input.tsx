3b0930022c1dbcf2a0849821f356ea63bf9fe1b5
Mary <marieshix98@gmail.com>
2024-05-13 23:43:27 +0300

    handle input and select user input

--- src/app/_components/currency-input.tsx
+++ src/app/_components/currency-input.tsx
@@ -1,21 +1,52 @@
+import { ChangeEvent } from "react";
+
 interface Props {
   amountToSend: string | number;
-  currencies?: string[];
-  onChange: (x: any) => void;
+  currencies: string[];
+  onChangeAmount: (x: any) => void;
+  currency: string;
+  onChangeCurrency: (x: any) => void;
 }
 export default function CurrencyInput({
   amountToSend,
   currencies,
-  onChange,
+  onChangeAmount,
+  onChangeCurrency,
+  currency,
 }: Props) {
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value, "set value");
+    onChangeAmount(event.target.value);
+  };
+  const handleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    onChangeCurrency(event.target.value);
+  };
+
   return (
-    <div>
-      <input value={amountToSend} onChange={onChange} />
-      {/* <select>
-        {currencies.map((currency) => (
-          <option value={currency}>{currency}</option>
+    <div className="flex">
+      <input
+        value={amountToSend}
+        type="text"
+        onChange={handleInput}
+        className="bg-gray-50 w-[80%] border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-right-none"
+      />
+      <select
+        value={currency}
+        onChange={handleSelect}
+        className="bg-gray-50 w-[20%] border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        {currencies.map((value) => (
+          <option
+            value={value.split(" ")[1]}
+            className="flex gap-8 h-auto items-center"
+          >
+            <div className="w-[10px] h-[10px] bg-blue-600 rounded-full">
+              {value.split(" ")[0]}
+            </div>
+            <p className="text-red-400">{value.split(" ")[1]}</p>
+          </option>
         ))}
-      </select> */}
+      </select>
     </div>
   );
 }
