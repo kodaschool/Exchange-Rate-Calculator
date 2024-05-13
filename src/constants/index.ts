922502ed2dd016b16a0ab3033bae61871649005e
Mary <marieshix98@gmail.com>
2024-05-13 23:42:53 +0300

    switch to use fixer currency api

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,7 @@ const BASE_URL_COUNTRIES = "https://restcountries.com/v3.1/all";
 const CURRENCY_URL = "https://free.currconv.com";
 const EXCHANGE_RATE_URL =
   "https://v6.exchangerate-api.com/v6/YOUR_KEY/latest/USD";
-export { BASE_URL_COUNTRIES, CURRENCY_URL };
+const FREE_CURRENCY_URL = "https://free.currconv.com";
+const FIXER_CURRENCY_API =
+  "http://data.fixer.io/api/latest?access_key=048972284dc15f1aa2b4bcfc3da567d05";
+export { BASE_URL_COUNTRIES, CURRENCY_URL, FIXER_CURRENCY_API };
